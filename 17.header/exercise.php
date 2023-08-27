d97acb96250fafbb18725b0156c59b6a25345ac1
KSHDestiny <kaungsathein.5899@gmail.com>
2023-08-27 18:13:39 +0630

    Header

--- 17.header/exercise.php
+++ 17.header/exercise.php
@@ -1,17 +1,23 @@
 <?php
     // header("location: filepath")
 
-    $x = 10;
-
     $userData = [
         'name' => "Destiny",
         'age' => 23,
         "job" => "developer"
     ];
 
-    if($x == 10){
-        header("location: read.php?name={$userData['name']}");
+    if(true){
+        // header("location: read.php?name={$userData['name']}");
+
+        // header("Context-Type: application/pdf");
+        // header("Cache-Control: no-cache");
+        header( "refresh:5;url=read.php?name={$userData['name']}" );
+        echo "You will be redirected in about 5 secs. If not, click <a href='read.php?name={$userData["name"]}'>here</a>.";
+
     }
+
+
 
     echo "This is exercise page.";
 ?>
